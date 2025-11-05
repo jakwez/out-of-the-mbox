38eefdd0b41455d067fe2b59ba2630ee4a553d2d
jakwez <jmenuet+gh@gmail.com>
2025-11-05 11:30:00 -0800

    Nicer looking avatar circle with initials and color

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,65 @@ const VisuallyHiddenInput = styled("input")({
   width: 1,
 });
 
+// From MUI documentation https://mui.com/material-ui/react-avatar/
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = "#";
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+  return color;
+}
+
+function getInitials(name: string): string {
+  // Remove punctuation and extra whitespace
+  const clean = name.replace(/[^\p{L}\p{N}\s]/gu, "").trim();
+  // Split on spaces and filter out empty parts
+  const parts = clean.split(/\s+/).filter(Boolean);
+  // Take first character of each part and capitalize
+  return parts.map((p) => p[0].toUpperCase()).join("");
+}
+
+function stringAvatar(name: string) {
+  // let initials: string;
+  // if (name.length > 0) {
+  //   const nameParts = name.split(" ");
+  //   for ( let i=0; i<2; i++ ) {
+  //     if ( nameParts[i])
+
+  //   }
+  //   if (nameParts.length >= 2) {
+  //     initials = `${nameParts[0][0]}${nameParts[1][0]}`;
+  //   } else {
+  //     initials = name[0];
+  //   }
+  // } else {
+  //   initials = "?";
+  // }
+  const allInitials = getInitials(name);
+  let twoInitials: string;
+  if (allInitials.length >= 2) {
+    twoInitials = allInitials[0] + allInitials[1];
+  } else if (allInitials.length == 1) {
+    twoInitials = allInitials[0];
+  } else {
+    twoInitials = "?";
+  }
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: twoInitials,
+  };
+}
+
 // MBOX email index to actual Email record
 // type MBOXEmails = Record<number, Email>;
 
@@ -85,7 +144,7 @@ function createListItem(email: Email, key: string) {
     <Fragment key={key}>
       <ListItem alignItems="flex-start">
         <ListItemAvatar>
-          <Avatar alt={name} src="/static/images/avatar/1.jpg" />
+          <Avatar {...stringAvatar(name)} />
         </ListItemAvatar>
         <ListItemText
           primary={email.subject ?? "(No subject)"}
