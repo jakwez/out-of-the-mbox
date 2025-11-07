24f9f92fe7674decee86414574972c1dc600d93b
jakwez <jmenuet+gh@gmail.com>
2025-11-07 10:15:03 -0800

    First pass integration of dompurify to sanitize html before displaying it

--- src/components/EmailDialog.tsx
+++ src/components/EmailDialog.tsx
@@ -1,10 +1,17 @@
 import {
+  Box,
   Dialog,
   DialogContent,
   DialogContentText,
   DialogTitle,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  Typography,
 } from "@mui/material";
+import DOMPurify from "dompurify";
 import type { Email } from "postal-mime";
+import { useState } from "react";
 export interface EmailDialogProps {
   email: Email;
   open: boolean;
@@ -12,7 +19,15 @@ export interface EmailDialogProps {
 }
 
 export function EmailDialog(props: EmailDialogProps) {
-  const viewHtml = false;
+  const [viewHtml, setViewHtml] = useState(true);
+  const [purifyHtml, setPurifyHtml] = useState(true);
+  let html = purifyHtml
+    ? DOMPurify.sanitize(props.email.html!, {
+        FORBID_TAGS: ["iframe", "script", "link", "object", "embed"],
+        FORBID_ATTR: ["srcset", "xlink:href", "formaction"],
+        ALLOWED_URI_REGEXP: /^data:/, // only allow inline data URIs
+      })
+    : props.email.html!;
   return (
     <Dialog
       open={props.open}
@@ -20,10 +35,36 @@ export function EmailDialog(props: EmailDialogProps) {
       fullWidth={true}
       maxWidth={"lg"}
     >
-      <DialogTitle>{props.email.subject!}</DialogTitle>
-      <DialogContent>
+      <DialogTitle>
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Typography variant="h6">{props.email.subject!}</Typography>
+          <FormGroup sx={{ flexDirection: "row" }}>
+            <FormControlLabel
+              control={
+                <Switch
+                  size={"small"}
+                  checked={viewHtml}
+                  onChange={(event) => setViewHtml(event.target.checked)}
+                />
+              }
+              label="View HTML"
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  size={"small"}
+                  checked={purifyHtml}
+                  onChange={(event) => setPurifyHtml(event.target.checked)}
+                />
+              }
+              label="Clean HTML"
+            />
+          </FormGroup>
+        </Box>
+      </DialogTitle>
+      <DialogContent sx={{ minHeight: 300 }}>
         {viewHtml ? (
-          <div dangerouslySetInnerHTML={{ __html: props.email.html! }} />
+          <div dangerouslySetInnerHTML={{ __html: html }} />
         ) : (
           <DialogContentText variant="caption">
             {props.email.text!}
